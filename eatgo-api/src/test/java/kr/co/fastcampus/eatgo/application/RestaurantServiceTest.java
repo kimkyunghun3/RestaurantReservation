b80e8fd99852ce1634b1fb2de2473e915b061e2a
kimkyunghun3 <kimkyunghun3@naver.com>
2020-03-12 01:01:33 +0900

    가짜 객체를 이용하여 Repository 구분

--- eatgo-api/src/test/java/kr/co/fastcampus/eatgo/application/RestaurantServiceTest.java
+++ eatgo-api/src/test/java/kr/co/fastcampus/eatgo/application/RestaurantServiceTest.java
@@ -3,28 +3,55 @@ package kr.co.fastcampus.eatgo.application;
 import kr.co.fastcampus.eatgo.domain.*;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
+import static org.mockito.BDDMockito.given;
 
 public class RestaurantServiceTest {
 
     private RestaurantService restaurantService;
 
+    @Mock
     private RestaurantRepository restaurantRepository;
 
+    @Mock
     private MenuItemRepository menuItemRepository;
 
     @Before
     public void setUp(){
-        restaurantRepository = new RestaurantRepositoryImpl();
-        menuItemRepository = new MenuItemRepositoryImpl();
 
-        restaurantService = new RestaurantService(restaurantRepository, menuItemRepository);
+        MockitoAnnotations.initMocks(this);  //Mock붙어있는 것들 초기화 시켜준다
+
+        mockRestaurantRepository();
+        mockMenuItemRepository();
+//        restaurantRepository = new RestaurantRepositoryImpl();
+//        menuItemRepository = new MenuItemRepositoryImpl();
+
+        restaurantService = new RestaurantService(
+                restaurantRepository, menuItemRepository);
     }
 
+
+
+    private void mockRestaurantRepository() {
+        List<Restaurant> restaurants = new ArrayList<>();
+        Restaurant restaurant = new Restaurant(1004L,"Bob zip","Seoul");
+        restaurants.add(restaurant);
+        given(restaurantRepository.findAll()).willReturn(restaurants);
+
+        given(restaurantRepository.findById(1004L)).willReturn(restaurant);
+    }
+    private void mockMenuItemRepository() {
+        List<MenuItem> menuItems = new ArrayList<>();
+        menuItems.add(new MenuItem("Kimchi"));
+        given(menuItemRepository.findAllByRestaurantId(1004L)).willReturn(menuItems);
+    }
     @Test
     public void getRestaurants() {
         List<Restaurant> restaurants = restaurantService.getRestaurants();
