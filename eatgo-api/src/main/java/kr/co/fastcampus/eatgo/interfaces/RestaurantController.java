f71dad21b27c5551cbcce2938a412bc5c3e7f4b1
kimkyunghun3 <kimkyunghun3@naver.com>
2020-03-11 18:03:35 +0900

    가게 상세를 불러오는 API 일부 구현

--- eatgo-api/src/main/java/kr/co/fastcampus/eatgo/interfaces/RestaurantController.java
+++ eatgo-api/src/main/java/kr/co/fastcampus/eatgo/interfaces/RestaurantController.java
@@ -2,6 +2,7 @@ package kr.co.fastcampus.eatgo.interfaces;
 
 import kr.co.fastcampus.eatgo.domain.Restaurant;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -14,10 +15,30 @@ public class RestaurantController {
     public List<Restaurant> list() {
         List<Restaurant> restaurants = new ArrayList<>();
 
-        Restaurant restaurant = new Restaurant(1004L,"Bob zip", "Seoul");
+        Restaurant restaurant = new Restaurant(1004L, "Bob zip", "Seoul");
 
         restaurants.add(restaurant);
 
         return restaurants;
+    }
+
+    @GetMapping("/restaurants/{id}")
+    public Restaurant detail(@PathVariable("id") Long id) {
+        List<Restaurant> restaurants = new ArrayList<>();
+
+        restaurants.add(new Restaurant(1004L,"Bob zip", "Seoul"));
+        restaurants.add(new Restaurant(2020L, "Cyber Food", "Seoul"));
+        Restaurant restaurant = restaurants.stream()
+                .filter(r -> r.getId().equals(id))
+                .findFirst()
+                .orElse(null);  //찾을 수 없는 경우까지 만들 때 이용( 필요 없을 시 .get()으로 한다)
+
+//        if (id == 1004L) {
+//            restaurant = new Restaurant(1004L, "Bob zip", "Seoul");
+//        }
+//        if (id == 2020L) {
+//            restaurant = new Restaurant(2020L, "Cyber Food", "Seoul");
+//        }
+        return restaurant;
     }
 }
